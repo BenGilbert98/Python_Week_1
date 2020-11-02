6907784d1a0c64de384176ae508b6c5119b35300
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-02 16:58:14 +0000

    Slicing, strip, count and other operators

--- strings_concatenation_&_casting.py
+++ strings_concatenation_&_casting.py
@@ -17,12 +17,39 @@
 #
 # last_name_2 = int("777")
 # print(last_name_2, type(last_name_2))
+#
+# name = input("Please enter your name    ")
+# age = input("Please enter your age    ")
+# house_number = input("What is your house number?    ")
+# address = input("What is the 1st line of your address?    ")
+# post_code = input("What is your post code?    ")
 
-name = input("Please enter your name    ")
-age = input("Please enter your age    ")
-house_number = input("What is your house number?    ")
-address = input("What is the 1st line of your address?    ")
-post_code = input("What is your post code?    ")
 
+# print("Hello", name + "!", "you are", age, "years old", "and you live at", house_number, address + ",", post_code)
 
-print("Hello", name + "!", "you are", age, "years old", "and you live at", house_number, address + ",", post_code)
+# single_quotes = 'Single \'wow\' Quotes'
+# print (single_quotes)
+#
+# double_quotes = "Double 'wow' Quotes"
+# print (double_quotes)
+
+# String Slicing - why -
+
+# greetings = "Hello World!"
+# print(len(greetings)) # prints the length of the string
+# print(greetings[0:6]) # prints Hello
+# print(greetings[6:11]) # prints World
+# print(greetings[-1]) # prints !
+
+# Strip() removes white spaces at beginning and end of a string
+# white_spaces = "     lots of spaces                  "
+# print(len(white_spaces))
+# print(len(white_spaces.strip()))
+
+# count() it counts the number of times any word is available in the string
+# example_text = "Lots of text with Some text"
+# print(example_text.count("text"))
+# print(example_text.replace("Lots","Not Lots"))
+# print(example_text.capitalize())
+# print(example_text.upper())
+# print(example_text.lower())
