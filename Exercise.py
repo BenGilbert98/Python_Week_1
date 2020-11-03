77490de466996c4193b1a170842f733c7578f568
BenGilbert98 <73166552+BenGilbert98@users.noreply.github.com>
2020-11-03 09:24:51 +0000

    Update Exercise.py

--- Exercise.py
+++ Exercise.py
@@ -1,12 +1,6 @@
 name = input("What is your name?     ")
 dob = input("What is your date of birth?    (dd-mm-yyyy)     ")
-
-try:
-    age = int(input("How old are you?    "))
-    if age <= 0:
-        raise ValueError("Your age cannot be 0 or below")
-except ValueError as err:
-    print("Please enter a valid age, {}".format(err))
-else:
-    print (name, ", ", dob, ", ", age)
-    print(type(name), ", ", type(dob), ", ", type(age))
+age = int(input("How old are you?    "))
+ 
+print (name, ", ", dob, ", ", age)
+print(type(name), ", ", type(dob), ", ", type(age))
